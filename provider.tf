1ddb29ff7e15d057026eee226a7ef51101f52729
mahmoudadisi <m.ad42@yahoo.com>
2025-07-26 03:45:56 +0300

    second project test

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-  shared_config_files      = ["/Users/enggalal/.aws/config"]
-  shared_credentials_files = ["/Users/enggalal/.aws/credentials"]
+  shared_config_files      = ["/home/adidi/.aws/config"]
+  shared_credentials_files = ["/home/adidi/.aws/credentials"]
   profile                  = "terraform_dev"
   region                   = "us-east-1"
 }
