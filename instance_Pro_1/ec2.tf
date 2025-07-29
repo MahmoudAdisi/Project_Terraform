315031d06b82376e6c9672bb39bdea3c100da59b
mahmoudadisi <m.ad42@yahoo.com>
2025-07-29 20:59:52 +0300

    project test ec2

---
+++ instance_Pro_1/ec2.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+    region = "us-este-1"  
+}
+resource "aws_instance" "myec2" {
+    ami = "ami-0150ccaf51ab55a51"
+    instance_type = "t2.micro"
+    tags = {
+    name="sintinal_check"
+    }
+}
