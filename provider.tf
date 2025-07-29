290e44061f869f0023c4aaa7f4d7058dd6dd6596
mahmoudadisi <m.ad42@yahoo.com>
2025-07-29 21:09:34 +0300

    delete some file project test ec2

--- provider.tf
+++
@@ -1,6 +0,0 @@
-provider "aws" {
-  shared_config_files      = ["/home/adidi/.aws/config"]
-  shared_credentials_files = ["/home/adidi/.aws/credentials"]
-  profile                  = "terraform_dev"
-  region                   = "us-east-1"
-}
