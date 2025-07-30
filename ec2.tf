10d329716c75da6887fb15c75da17d6eba6aec9f
MahmoudAdisi <Mahmoud.aam@outlook.com>
2025-07-30 22:53:00 +0300

    Update ec2.tf small

--- ec2.tf
+++ ec2.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 resource "aws_instance" "myec2" {
     ami = "ami-0150ccaf51ab55a51"
-    instance_type = "t2.micro"
+    instance_type = "t2.small"
     tags = {
         name="sintinal_check"
     }
